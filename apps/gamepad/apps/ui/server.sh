0b7277401d19d57c9c76071a7e0eead59592b87d
rob-brown <ammoknight@gmail.com>
2023-03-11 21:53:29 -0700

    Fix bugs with startup

--- apps/gamepad/apps/ui/server.sh
+++ apps/gamepad/apps/ui/server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-env MIX_ENV=prod PHX_SERVER=true SECRET_KEY_BASE=frgvEBs7NY74auQ8ziMVeEIB8XUOYKFs7PfNfpns+1CA3+e+DH5nUHGq1UExr3DH PHX_HOST=ammobox.local DB_PATH=/home/pi/amiibo.sqlite /home/pi/gamepad/apps/ui/_build/prod/rel/ui/bin/server
+if [[ $# -ne 1 ]]; then
+    echo "Expected one argument with the platform (joybonnet or ammobox)"
+    exit 2
+fi
+
+sudo killall pigpiod
+sudo env PLATFORM=$1 MIX_ENV=prod PHX_SERVER=true SECRET_KEY_BASE=frgvEBs7NY74auQ8ziMVeEIB8XUOYKFs7PfNfpns+1CA3+e+DH5nUHGq1UExr3DH PHX_HOST=ammobox.local DB_PATH=/home/pi/amiibo.sqlite /home/pi/amiibo_system/apps/gamepad/apps/ui/_build/prod/rel/ui/bin/server
