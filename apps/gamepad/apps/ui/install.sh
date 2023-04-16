35e745b8886366314096aef9f8f6053506b05e81
rob-brown <ammoknight@gmail.com>
2023-04-16 14:05:14 -0600

    Reorder install script

--- apps/gamepad/apps/ui/install.sh
+++ apps/gamepad/apps/ui/install.sh
@@ -16,10 +16,10 @@ curl --location $URL > ~/gamepad/gamepad.tar.gz
 echo Uncompressing release files
 tar -xzf ~/gamepad/gamepad.tar.gz -C ~/gamepad
 
-echo Setting up database
-env MIX_ENV=prod DB_PATH=$DATABASE SECRET_KEY_BASE=ignore /home/pi/gamepad/bin/gamepad eval "Ui.Release.migrate"
-
 echo Installing startup script
 echo "@reboot /usr/bin/env PHX_HOST=ammobox.local PORT=4000 DB_PATH=$DATABASE PLATFORM=$PLATFORM SECRET_KEY_BASE=frgvEBs7NY74auQ8ziMVeEIB8XUOYKFs7PfNfpns+1CA3+e+DH5nUHGq1UExr3DH /home/pi/gamepad/bin/gamepad daemon" | sudo tee /etc/cron.d/gamepad >> /dev/null
+
+echo Setting up database
+env MIX_ENV=prod DB_PATH=$DATABASE SECRET_KEY_BASE=ignore /home/pi/gamepad/bin/gamepad eval "Ui.Release.migrate"
 
 echo Reboot your Raspberry Pi to start the gamepad
