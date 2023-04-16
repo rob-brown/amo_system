429748a9e5539776afd482e1d118c666759230bf
rob-brown <ammoknight@gmail.com>
2023-04-16 14:40:49 -0600

    Set user for startup script

--- apps/gamepad/apps/ui/install.sh
+++ apps/gamepad/apps/ui/install.sh
@@ -17,7 +17,7 @@ echo Uncompressing release files
 tar -xzf ~/gamepad/gamepad.tar.gz -C ~/gamepad
 
 echo Installing startup script
-echo "@reboot /usr/bin/env PHX_SERVER=true PHX_HOST=ammobox.local PORT=4000 DB_PATH=$DATABASE PLATFORM=$PLATFORM SECRET_KEY_BASE=frgvEBs7NY74auQ8ziMVeEIB8XUOYKFs7PfNfpns+1CA3+e+DH5nUHGq1UExr3DH /home/pi/gamepad/bin/gamepad daemon" | sudo tee /etc/cron.d/gamepad >> /dev/null
+echo "@reboot pi sudo /usr/bin/env PHX_SERVER=true PHX_HOST=ammobox.local PORT=4000 DB_PATH=$DATABASE PLATFORM=$PLATFORM SECRET_KEY_BASE=frgvEBs7NY74auQ8ziMVeEIB8XUOYKFs7PfNfpns+1CA3+e+DH5nUHGq1UExr3DH /home/pi/gamepad/bin/gamepad start" | sudo tee /etc/cron.d/gamepad >> /dev/null
 
 echo Setting up database
 env MIX_ENV=prod DB_PATH=$DATABASE SECRET_KEY_BASE=ignore /home/pi/gamepad/bin/gamepad eval "Ui.Release.migrate"
