1d03e5023095025fe8d9b745d1ca89abb4d148e5
rob-brown <ammoknight@gmail.com>
2023-04-15 18:25:33 -0600

    Improve install script with output

--- apps/gamepad/apps/ui/install.sh
+++ apps/gamepad/apps/ui/install.sh
@@ -8,14 +8,15 @@ fi
 PLATFORM=$1
 URL="https://github.com/rob-brown/amiibo_system/releases/latest/download/gamepad.tar.gz"
 
-# Download release
-mkdir ~/gamepad
+echo Downloading latest gamepad release
+mkdir -p ~/gamepad
 curl --location $URL > ~/gamepad/gamepad.tar.gz
 
-# Unzip
+echo Uncompressing release files
 tar -xzf ~/gamepad/gamepad.tar.gz -C ~/gamepad
 
 # Update crontab so the code starts on boot
+echo Installing startup script
 echo "@reboot /usr/bin/env PLATFORM=$PLATFORM SECRET_KEY_BASE=frgvEBs7NY74auQ8ziMVeEIB8XUOYKFs7PfNfpns+1CA3+e+DH5nUHGq1UExr3DH /home/pi/gamepad/bin/gamepad daemon" | sudo tee /etc/cron.d/gamepad >> /dev/null
 
 echo Reboot your Raspberry Pi to start the gamepad
