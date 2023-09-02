eeda3c692ef6d114a74bfcf6d06da413c6d0bb58
Robert Brown <ammoknight@gmail.com>
2023-09-01 23:46:08 -0600

    Add env var to build script

--- apps/rabbit_driver/build.sh
+++ apps/rabbit_driver/build.sh
@@ -5,6 +5,6 @@ VERSION=$(cat "VERSION")
 echo Building Rabbit Driver v$VERSION
 
 mix deps.get --only prod
-env MIX_ENV=prod mix compile
-env MIX_ENV=prod mix release --overwrite
+env MIX_ENV=prod MIX_TARGET=rpi mix compile
+env MIX_ENV=prod MIX_TARGET=rpi mix release --overwrite
 cp _build/prod/rabbit_driver-$VERSION.tar.gz ./rabbit_driver-$VERSION.tar.gz
