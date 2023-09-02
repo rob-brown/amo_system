58afd9fc287e263516bbeb0e9146a1afc92bcb38
Robert Brown <ammoknight@gmail.com>
2023-09-02 00:44:16 -0600

    Fix target setting

--- apps/rabbit_driver/config/runtime.exs
+++ apps/rabbit_driver/config/runtime.exs
@@ -1,5 +1,6 @@
 import Config
 
-target = System.get_env("MIX_TARGET", "host") |> String.to_atom()
+target = System.get_env("MIX_TARGET") || System.get_env("TARGET") || "host"
+target = String.to_atom(target)
 
 config :rabbit_driver, :target, target
