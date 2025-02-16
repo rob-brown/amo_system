a7cbff092899d1edaba8bf55998525cb8d7ebb23
Robert Brown <ammoknight@gmail.com>
2025-02-15 20:18:38 -0700

    Add character names

--- apps/ssbu/mix.exs
+++ apps/ssbu/mix.exs
@@ -4,7 +4,7 @@ defmodule Ssbu.MixProject do
   def project do
     [
       app: :ssbu,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
